bd0b82d3ea5c2f417bb48bc933dc72573c3cd9fd
Tiziano Montenegro <91438170+tizielpro@users.noreply.github.com>
2022-07-20 21:50:35 -0300

    las ;

--- codigo.js
+++ codigo.js
@@ -7,21 +7,21 @@ while(isNaN(number)||number<0){
     number = parseInt(prompt("introduce un número",1));
 }
 
-document.write("Tabla pedida <br>")
+document.write("Tabla pedida <br>");
 document.write("<b>La Tabla del "+number+"</b><br>");
 for(var i = 1; i <= 10; i++){
     document.write(`${number} x ${i} = ${(number*i)}<br>`);
 }
-document.write("<br>")
-document.write("<br>")
+document.write("<br>");
+document.write("<br>");
 
 
 // para ver mas tablas de forma automatica.
-document.write("Todas las tablas <br>")
+document.write("Todas las tablas <br>");
 for(var number = 1; number <= 10; number++){
     document.write("<b>La Tabla del "+number+"</b><br>");
     for(var i = 1; i <= 10; i++){
-        document.write(`${number} x ${i} = ${(number*i)}<br>`)
+        document.write(`${number} x ${i} = ${(number*i)}<br>`);
     }
-    document.write("<br>")
+    document.write("<br>");
 }
